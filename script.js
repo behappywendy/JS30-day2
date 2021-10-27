6eeb27be805fde684af5f0254ced3ac1b0e2a4ad
wendy.liu <aaa3520732@gmail.com>
2021-10-27 10:36:46 +0800

    add text clock time

--- script.js
+++ script.js
@@ -16,21 +16,36 @@
 
         hour.style.transform = `rotate(${hourDeg})deg`;
     }
+
+    function textTime() {
+        const date = new Date();
+        const span = document.querySelectorAll('.displayTime span');
+        let hourNode = span[0];
+        let minNode = span[1];
+        let secNode = span[2];
+        hourNode.textContent = `${date.getHours()}點`;
+        minNode.textContent = `${date.getMinutes()}分`;
+        secNode.textContent = `${date.getSeconds()}秒`;
+    }
     //方法一
     setClock(); //初始化
+    textTime();
     // 更新畫面:setInterVal、setTimeout、requestAnimationFrame
-    setInterval(setClock, 1000);
-    //方法二
-    // function timoutHandler() {
-    //     setClock();
-    //     setTimeout(timoutHandler, 1000);
-    // }
-    // setClock();
-    // setTimeout(timoutHandler, 1000);
-    //方法三
-    // function animationHandler() {
-    //     setClock();
-    //     window.requestAnimationFrame(animationHandler);
-    // }
-    // window.requestAnimationFrame(animationHandler);
+
+    // setInterval(setClock, 1000);
+    // setInterval(textTime, 1000);
 })();
+
+//方法二
+// function timoutHandler() {
+//     setClock();
+//     setTimeout(timoutHandler, 1000);
+// }
+// setClock();
+// setTimeout(timoutHandler, 1000);
+//方法三
+// function animationHandler() {
+//     setClock();
+//     window.requestAnimationFrame(animationHandler);
+// }
+// window.requestAnimationFrame(animationHandler);
